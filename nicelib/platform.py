f3ebe8eaffc8b2c405a365801ec0a18ab1bc101e
Nate Bogdanowicz <natezb@gmail.com>
2017-02-17 10:30:25 -0800

    Add replacement of "signed __intx" for windows

--- nicelib/platform.py
+++ nicelib/platform.py
@@ -82,12 +82,16 @@ elif COMPILER == 'MSVC':
     # Ordered by precedence - should usually be longest match first
     REPLACEMENT_MAP = [
         (['unsigned', '__int8'], 'uint8_t'),
+        (['signed', '__int8'], 'int8_t'),
         (['__int8'], 'int8_t'),
         (['unsigned', '__int16'], 'uint16_t'),
+        (['signed', '__int16'], 'int16_t'),
         (['__int16'], 'int16_t'),
         (['unsigned', '__int32'], 'uint32_t'),
+        (['signed', '__int32'], 'int32_t'),
         (['__int32'], 'int32_t'),
         (['unsigned', '__int64'], 'uint64_t'),
+        (['signed', '__int64'], 'int64_t'),
         (['__int64'], 'int64_t'),
     ]
 
