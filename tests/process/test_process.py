8dd6f8be71891a18dac38c7c368d2478a884ce1e
Nate Bogdanowicz <natezb@gmail.com>
2018-02-09 18:38:20 -0800

    Fix tests broken by change of process_headers's return vals

--- tests/process/test_process.py
+++ tests/process/test_process.py
@@ -3,7 +3,7 @@ from nicelib.process import process_headers
 
 
 def test_exclude_sys_funcdefs():
-    header_src, _ = process_headers(local_fpath(__file__, 'general/sys-funcdef-a.h'))
+    header_src, _, _ = process_headers(local_fpath(__file__, 'general/sys-funcdef-a.h'))
     assert 'program_func' in header_src
     assert 'created_func' in header_src
     assert 'sys_func' not in header_src
