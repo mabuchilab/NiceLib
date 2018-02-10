f87d8ebdd8dd76a51171afaa026d56100e4ce7e5
Nate Bogdanowicz <natezb@gmail.com>
2018-02-09 18:38:20 -0800

    Fix tests broken by change of process_headers's return vals

--- tests/process/test_enum_values.py
+++ tests/process/test_enum_values.py
@@ -4,7 +4,7 @@ from nicelib.process import process_headers
 
 
 def test_func_macro():
-    header_src, _ = process_headers(local_fpath(__file__, 'enum/func-macro.h'))
+    header_src, _, _ = process_headers(local_fpath(__file__, 'enum/func-macro.h'))
     assert re.search(r"X\s*=\s*256", header_src)
     assert re.search(r"Y\s*=\s*512", header_src)
     assert re.search(r"Z\s*=\s*0", header_src)
