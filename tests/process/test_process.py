315ef427ee5acb9a7ee7450a26c5a5d783f44cda
Nate Bogdanowicz <natezb@gmail.com>
2018-02-09 18:37:47 -0800

    Add test of argname parsing

--- tests/process/test_process.py
+++ tests/process/test_process.py
@@ -7,3 +7,20 @@ def test_exclude_sys_funcdefs():
     assert 'program_func' in header_src
     assert 'created_func' in header_src
     assert 'sys_func' not in header_src
+
+
+def test_argnames():
+    _, _, argnames = process_headers(local_fpath(__file__, 'general/argnames.h'))
+    assert argnames['f'] is None
+    assert argnames['f_void'] == []
+    assert argnames['f_int'] == [None]
+    assert argnames['f_int_int'] == [None, None]
+
+    assert argnames['f_inta'] == ['a']
+    assert argnames['f_inta_intb'] == ['a', 'b']
+
+    assert argnames['f_ptra'] == ['a']
+    assert argnames['f_arra'] == ['a']
+
+    assert argnames['f_structa'] == ['a']
+    assert argnames['f_structptra'] == ['a']
