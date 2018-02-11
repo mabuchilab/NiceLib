671112016e88204c5f05603a39e5df2eea01e9fc
Nate Bogdanowicz <natezb@gmail.com>
2018-02-11 09:56:09 -0800

    Add end-to-end test using tiny shared library

---
+++ tests/midlevel/test_foo.py
@@ -0,0 +1,19 @@
+from nicelib import NiceLib, load_lib
+from nicelib.nicelib import Sig, NiceObject, ret_ignore, ret_return
+
+
+class NiceFoo(NiceLib):
+    """Foo library"""
+    _info = load_lib('foo', pkg=None, dir=__file__)
+    _ret = ret_return
+
+    add = Sig('in', 'in')
+    create_item = Sig()
+
+    class Item(NiceObject):
+        _init_ = 'create_item'
+        _prefix_ = 'item_'
+
+        get_id = Sig('in')
+        get_value = Sig('in', ret=ret_ignore)
+        set_value = Sig('in', 'in')
