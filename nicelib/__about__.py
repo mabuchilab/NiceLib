ff4683bc9250a8965be9b7e9f22386fa1332a2a3
Nate Bogdanowicz <natezb@gmail.com>
2018-07-09 17:19:26 -0700

    Let's try 0.5.2...

--- nicelib/__about__.py
+++ nicelib/__about__.py
@@ -3,7 +3,7 @@
 import datetime
 
 __distname__ = "NiceLib"
-__version__ = "0.5.1"
+__version__ = "0.5.2"
 __author__ = "Nate Bogdanowicz"
 __email__ = "natezb@gmail.com"
 __url__ = 'https://github.com/mabuchilab/NiceLib'
