ec80d9c61fe5ebb2051f068d6d789b43984fc52d
Nate Bogdanowicz <natezb@gmail.com>
2019-02-28 22:53:57 -0800

    Return to dev

--- nicelib/__about__.py
+++ nicelib/__about__.py
@@ -3,7 +3,7 @@
 import datetime
 
 __distname__ = "NiceLib"
-__version__ = "0.6"
+__version__ = "0.7.dev0"
 __author__ = "Nate Bogdanowicz"
 __email__ = "natezb@gmail.com"
 __url__ = 'https://github.com/mabuchilab/NiceLib'
